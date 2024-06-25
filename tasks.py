4ff9a63bdc705f93e4d0b5d235f220543903f010
oushesh <oushesh@gmail.com>
2024-06-25 19:34:38 +0200

    added sls_cert

--- tasks.py
+++ tasks.py
@@ -11,11 +11,26 @@ load_dotenv()
 #For production use redis_TLS_URL
 redis_url = os.getenv('REDIS_TLS_URL','rediss://:pd2d5a49e95897e1b3d0f0f8fbb43cffad27226cc3c12aa9c8622f240e716683a@ec2-44-206-187-141.compute-1.amazonaws.com:17720')
 
+
+# Ensure ssl_cert_reqs parameter is set correctly
+redis_ssl_cert_reqs = os.getenv('REDIS_SSL_CERT_REQS', 'CERT_REQUIRED')
+
+celery_app = Celery(
+    'tasks',
+    broker=redis_tls_url,
+    backend=redis_tls_url,
+    broker_transport_options={
+        'ssl_cert_reqs': redis_ssl_cert_reqs
+    },
+)
+
+"""
 celery_app = Celery(
     'tasks',
     broker=redis_url,
     backend=redis_url,
 )
+"""
 
 @celery_app.task
 def add(x, y):
